b771dacee203eaea5f3ba25ac5d2dec35f58ff92
Pedro Henrique Martins <ph.martins89@gmail.com>
2022-05-24 14:56:15 -0300

    Construtores

---
+++ OrientacaoObjetos/EstoqueProdutoConstrutores/Produto.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace EstoqueProdutoConstrutores {
+    class Produto {
+        
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade){
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        } 
+
+        public double ValoreTotalEmEstoque(){ //METODOS
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade){ //QUANDO É VARIAVEL DE PARAMETRO DE ENTRADA O PADRAO É LETRA MINUSCULA
+            Quantidade += quantidade;
+        }
+
+
+        public void RemoverProdutos(int quantidade){ //QUANDO É VARIAVEL DE PARAMETRO DE ENTRADA O PADRAO É LETRA MINUSCULA
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString() //OVERRIDE É UMA OPERAÇAO DE OUTRA CLASE A OBJETCT
+        { 
+            return Nome //REAPROVIETAMENTO DE CÓDIGO EM CONCATENAÇÃOs
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValoreTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ OrientacaoObjetos/EstoqueProdutoConstrutores/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstoqueProdutoConstrutores{
+    class Program{
+        static void Main(string[] args){
+
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine(); // LER UMA STRING
+            Console.WriteLine("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); // LER UM DOUBLE
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine()); // LER UM INT
+
+            Produto p = new Produto(nome,preco,quantidade); // Construtor
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produto a ser adicionado: ");
+            int qte = int.Parse(Console.ReadLine());            
+            p.AdicionarProdutos(qte); 
+
+            Console.WriteLine();  
+            Console.WriteLine("Dados atualizados: "+p);
+
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido: ");
+            qte = int.Parse(Console.ReadLine());            
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();  
+            Console.WriteLine("Dados atualizados: "+p);
+
+        }
+    }
+}
