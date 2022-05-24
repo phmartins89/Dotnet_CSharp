03dcab6d71a46df706244529c5ad195b233bd9db
Pedro Henrique Martins <ph.martins89@gmail.com>
2022-05-24 11:08:04 -0300

    Retira de espaco dos nomes de pasta e Exercicios

---
+++ OrientacaoObjetos/MembrosEstaticos/Exercicios/ConversorMoeda.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Exercicios {
+    
+    public class ConversorDeMoeda {
+
+
+        public static double Iof = 6.0;
+
+        public static double DolarParaReal(double quantia, double cotacao){
+            double total = quantia * cotacao;
+            return total + total * Iof / 100.00;
+        }
+    }
+}
---
+++ OrientacaoObjetos/MembrosEstaticos/Exercicios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios{
+    class Program{
+        static void Main(string[] args){
+
+
+            Console.WriteLine("Qual é a cotação do dolar?: ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dolares voce vai comprar?: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.DolarParaReal(quantia,cotacao);        
+            
+            Console.WriteLine("Valor a ser pago em reais: " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+        
+    }
+}
